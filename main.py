ba86e27e197bf49056a3e8e4434541429d55a895
johnadu <saintad22@gmail.com>
2021-09-25 10:24:07 -0400

    Replace the saving approach to an object format

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 title = input('enter your title: ')
 description = input('enter a description: ')
 todolist = []
-todolist.append({'title': title})
-todolist.append({'description': description})
+todo = {
+	"title": title,
+	"description": description,
+	'isCompleted': False
+}
+todolist.append(todo)
 print('This is my daily accomplishment')
 print(todolist)
