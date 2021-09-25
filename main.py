1990c0e19069b1711990e94f02dc15a7b67fc24b
John Adu <johnadu@Johns-MBP-2.hsd1.md.comcast.net>
2021-09-25 09:04:47 -0400

    initial commit of the application

---
+++ main.py
@@ -0,0 +1,7 @@
+title = 'this is my first task'
+description = "it's a to do list"
+todolist = []
+todolist.append({'title': title})
+todolist.append({'description': description})
+print('This is my daily accomplishment')
+print(todolist)
